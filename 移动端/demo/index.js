d0e8e2bafca34cdea6e79a0428de271ba136f079
dongxiuling <dxl_web@163.com>
2019-01-17 17:12:24 +0800

    demo

--- 移动端/demo/index.js
+++ 移动端/demo/index.js
@@ -6,11 +6,28 @@ var last = -1;
 $(document).swipeUp(function(){
     last = iNow;
     iNow++;
-    $('.page').eq(last).addClass('moveToTop');
-    $('.page').eq(iNow).removeClass('hide').addClass('moveFromBottom');
+    if(iNow <= $('.page').length-1){
+        $('.page').eq(last).attr('class','page page-'+ (last +1) +' moveToTop');
+        $('.page').eq(iNow).removeClass('hide').attr('class','page page-'+(iNow+1)+' moveFromBottom');
 
-    setTimeout(function(){
-        // 等iNow 当前页 600ms划出来后  再让图片出来开始执行动画
-        $('.page').eq(iNow).find('img').removeClass('hide');
-    },600)
+        setTimeout(function(){
+            // 等iNow 当前页 600ms划出来后  再让图片出来开始执行动画
+            $('.page').eq(iNow).find('img').removeClass('hide');
+            $('.page').eq(last).find('img').addClass('hide');
+        },600);
+    }
 })
+$(document).swipeDown(function(){
+    if(iNow >=1 ){
+        last = iNow;
+        iNow--;
+        $('.page').eq(last).attr('class','page page-'+ (last+1) +' moveToBottom');
+        $('.page').eq(iNow).attr('class','page page-'+ (iNow+1) +' moveFromTop');
+        setTimeout(function(){
+            // 等iNow 当前页 600ms划出来后  再让图片出来开始执行动画
+            $('.page').eq(iNow).find('img').removeClass('hide');
+            $('.page').eq(last).find('img').addClass('hide');
+        },600);
+    }
+})
+
