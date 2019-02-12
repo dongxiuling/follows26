3a76f94df691362913e3a02a145fb19bb2e431db
dongxiuling <dxl_web@163.com>
2019-02-12 15:31:12 +0800

    cli

--- vuecli3/src/App.vue
+++ vuecli3/src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <div id="app">
-    <router-view/>
+   
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
-      <router-link to="/mine">Mine</router-link>
+      <router-link to="/mine">Mine</router-link>|
+      <router-link to="/test">test</router-link>
     </div>
+
+    <router-view/>
   </div>
 </template>
 
--- vuecli3/src/router.js
+++ vuecli3/src/router.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Mine from './views/Mine.vue'
+import Test from './views/Test.vue'
+import A from './views/A.vue'
+import B from './views/B.vue'
+
 
 Vue.use(Router)
 
@@ -23,6 +27,20 @@ export default new Router({
     },{
       path:'/mine',
       component:Mine
-    }
+    },{
+      path:'/test',
+      component:Test,
+      children:[
+        {
+          path:'/a',
+          component:A
+        },
+        {
+          path:'/b',
+          component:B
+        }
+      ]
+    },
+
   ]
 })
