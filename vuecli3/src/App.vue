db9217741a87210e00db3f4a58f20629d45c264d
dongxiuling <dxl_web@163.com>
2019-02-12 16:38:03 +0800

    cli

--- vuecli3/src/App.vue
+++ vuecli3/src/App.vue
@@ -3,8 +3,9 @@
    
     <div id="nav">
       <router-link to="/">Home</router-link> |
+       <router-link to="/bb">Redict</router-link> |
       <!-- <router-link to="/about">About</router-link> | -->
-      <router-link to="/about/xx/5">About</router-link> |
+      <router-link to="/about/yy/5">About</router-link> |
       <!-- <router-link to="/mine">Mine</router-link>| -->
       <router-link :to="{name:'mine',params:{name:'zs',id:'001'}}">Mine</router-link>|
       <router-link to="/test">test</router-link>
