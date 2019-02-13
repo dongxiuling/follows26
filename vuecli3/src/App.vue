fb056f3f1b47983a0231f3b90fbf9190ecc23ed8
dongxiuling <dxl_web@163.com>
2019-02-13 09:38:55 +0800

    router

--- vuecli3/src/App.vue
+++ vuecli3/src/App.vue
@@ -3,15 +3,17 @@
    
     <div id="nav">
       <router-link to="/">Home</router-link> |
-       <router-link to="/bb">Redict</router-link> |
+      <router-link to="/abc">abc</router-link> |
+      <router-link to="/bb/xx/66">Redict</router-link> |
       <!-- <router-link to="/about">About</router-link> | -->
       <router-link to="/about/yy/5">About</router-link> |
       <!-- <router-link to="/mine">Mine</router-link>| -->
       <router-link :to="{name:'mine',params:{name:'zs',id:'001'}}">Mine</router-link>|
       <router-link to="/test">test</router-link>
     </div>
-
-    <router-view/>
+    <transition name="fade">
+      <router-view/>
+    </transition>
   </div>
 </template>
 
@@ -34,5 +36,25 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+.fade-enter{
+  /* opacity: 0; */
+  transform: translateX(-100%);
+}
+.fade-enter-active{
+  transition: transform 1s ease;
+}
+.fade-enter-to{
+  /* opacity: 1; */
+  transform: translateX(0);
+}
+.fade-leave{
+  opacity: 1;
+}
+.fade-leave-active{
+  transition: opacity 2s ease;
+}
+.fade-leave-to{
+  opacity: 0;
 }
 </style>
--- vuecli3/src/router.js
+++ vuecli3/src/router.js
@@ -21,11 +21,11 @@ export default new Router({
     {
       path: '/about/:name/:id',
       name: 'about',
-      // component:About
+      component:About
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      // component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     },{
       path:'/mine',
       name:'mine',
@@ -45,8 +45,12 @@ export default new Router({
       ]
     },
     {
-      path:'/bb',
-      redirect:'/'
+      path:'/bb/:name/:id',
+      redirect:'/about/:name/:id'
+    },{
+      path:'/',
+      component: Home,
+      alias:'/abc'
     }
 
   ]
