f690be610fbdfb91258722eb21760eac4a80ec02
dongxiuling <dxl_web@163.com>
2019-02-18 09:36:07 +0800

    app

--- appvuex/src/views/music/MusicList.vue
+++ appvuex/src/views/music/MusicList.vue
@@ -5,18 +5,42 @@
             artist: 'Silent Siren',
             src: 'http://up.mcyt.net/md5/53/OTk1MDE2NA_Qq4329912.mp3',
             pic: 'https://moeplayer.b0.upaiyun.com/aplayer/secretbase.jpg'
-        }"/>
+        }" :list="list"/>
     </div>
 </template>
 
 <script>
     import Aplayer from 'vue-aplayer';
+    import Axios from 'axios';
     export default {
+        data(){
+            return {
+                list:[]
+            }
+        },
+        computed:{
+            music(){
+                return this.list[0];
+            }
+        },
         components:{
             Aplayer
         },
         created () {
-            
+            Axios.get('/data/musicdata.json')
+            .then((result)=>{
+                result.data.musicData.forEach(obj => {
+                    let obj1 = {
+                        title:obj.title,
+                        artist:obj.author,
+                        src:obj.src,
+                        pic:obj.musicImgSrc,
+                        lrc:obj.lrc
+                    }
+                    this.list.push(obj1);
+                });
+                console.log(this.list)
+            })
         }
     }
 </script>
