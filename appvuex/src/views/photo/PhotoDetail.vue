3358839f6f550a6abd96877b9fdcf9904617089d
dongxiuling <dxl_web@163.com>
2019-02-20 11:56:53 +0800

    app

--- appvuex/src/views/photo/PhotoDetail.vue
+++ appvuex/src/views/photo/PhotoDetail.vue
@@ -3,18 +3,36 @@
         <!-- detail photo -->
         <!-- {{$route.params.index}} -->
 
-        {{photos[$route.params.index]}}
-        <div class="bg" :style="{background:'url('+ photos[$route.params.index].src +')'}"></div>
+        <!-- {{photos[$route.params.index]}} -->
+
+        <router-link to="/photo">
+            <v-touch v-on:swipeleft="next()" class="bg" :style="{backgroundImage:'url('+ photos[iNow].src +')'}"></v-touch>
+        </router-link>
     </div>
 </template>
 
 <script>
     import {mapState} from 'vuex';
+    import Vue from 'vue';
+    import VueTouch from 'vue-touch';
+    Vue.use(VueTouch, {name: 'v-touch'})
+
     export default {
+        data(){
+            return {
+                iNow:this.$route.params.index
+            }
+        },
         computed: {
             ...mapState(['photos'])
+        },
+        methods:{
+            next(){
+                this.iNow++;
+               
+                // console.log(this.$route.params.index);
+            }
         }
-        
     }
 </script>
 
@@ -25,7 +43,7 @@
         left:0px;
         bottom: 1rem;
         right:0px;
-        background: #000;
-
+        background: #000 no-repeat center center;
+        background-size: contain;
     }
 </style>
