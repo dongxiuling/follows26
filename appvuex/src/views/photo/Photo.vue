71c61f61d7719799941a929fe75cfb596d27f70c
dongxiuling <dxl_web@163.com>
2019-02-20 10:05:55 +0800

    app

--- appvuex/src/views/photo/Photo.vue
+++ appvuex/src/views/photo/Photo.vue
@@ -1,15 +1,35 @@
 <template>
-    <div>
-        图片
+    <div class="photo">
+        <img v-for="(photoSrc,index) in photo" :key="index" :src="photoSrc.src" @click="goDetail" alt="">
     </div>
 </template>
 
 <script>
+    import axios from 'axios'
     export default {
-        
+        data(){
+            return {
+                photo:[]
+            }
+        },
+        created () {
+            axios.get('/data/photodata.json')
+            .then((result)=>{
+                // console.log(result.data.photoData);
+                this.photo = [...result.data.photoData];
+
+            })
+        },
+        methods:{
+            goDetail(){
+                this.$router.push('/photodetail');
+            }
+        }
     }
 </script>
 
 <style scoped>
-
+    .photo img{
+        width:50%;
+    }
 </style>
