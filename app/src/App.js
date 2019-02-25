a6a5d961610e2643297ba868c4e756a3e8718f04
dongxiuling <dxl_web@163.com>
2019-02-25 14:21:57 +0800

    router

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route,Link} from 'react-router-dom'
+import Home from './components/Home.js';
+import A from './components/A.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <Router>
+              <div>
+                <Link to="/home">跳转home页</Link>|
+                <Link to="/a">跳转a页</Link>
+                <Route path="/home" component={Home}></Route>
+                <Route path="/a" component={A}></Route>
+              </div>
+          </Router>
       </div>
     );
   }
