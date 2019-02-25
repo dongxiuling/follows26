3800147f452dbdc297eeb3c0a12872090a5516ab
dongxiuling <dxl_web@163.com>
2019-02-25 15:06:24 +0800

    router

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route,Link} from 'react-router-dom'
-import Home from './components/Home.js';
-import A from './components/A.js';
+import { BrowserRouter as Router, Route,Link,Switch} from 'react-router-dom'
+import B from './components/B';
+import A from './components/A';
+import Home from './components/Home';
 
 class App extends Component {
   render() {
@@ -9,10 +10,20 @@ class App extends Component {
       <div className="App">
           <Router>
               <div>
-                <Link to="/home">跳转home页</Link>|
-                <Link to="/a">跳转a页</Link>
-                <Route path="/home" component={Home}></Route>
-                <Route path="/a" component={A}></Route>
+                <Link to="/">跳转home页</Link>|
+                <Link to="/b/10">跳转b页</Link>|
+                <Link to="/a/5">跳转a页</Link>
+
+                {/* <Route exact path="/" component={Home}></Route>
+                <Route path="/b" component={B}></Route>
+                <Route path="/a" component={A}></Route> */}
+                <Switch>
+                  <Route path="/b/:num" component={B}></Route>
+                  <Route path="/a/:id" component={A}></Route>
+                  <Route path="/" component={Home}></Route>
+                </Switch>
+
+
               </div>
           </Router>
       </div>
--- app/src/components/A.js
+++ app/src/components/A.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react'
 
-export default class Home extends Component {
+export default class A extends Component {
   render() {
+    console.log(this.props);
+    console.log(this.props.match.params.id);
+    let {match} = this.props.match;
     return (
       <div>
           A页
